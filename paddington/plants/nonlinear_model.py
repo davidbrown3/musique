cc134140e0f21f899fa0f76806ebaa1aaf8b0d47
David Browne <davidbrowne@Davids-MacBook-Pro.local>
2020-05-24 23:08:48 -0700

    Adding example nonlinear model

---
+++ paddington/plants/nonlinear_model.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+class InvertedPendulum():
+    '''
+    Example documented at:
+    https://coneural.org/florian/papers/05_cart_pole.pdf
+    '''
+
+    # u = force
+    # States = angular_position, angular_velocity, position, velocity
+
+    def __init__(self):
+        self.mass_pendulum = 5
+        self.mass_cart = 20
+        self.length = 1
+        self.gravity = 9.81
+        self.angular_friction = 0.1
+    
+    @property
+    def mass_total(self):
+        return self.mass_pendulum + self.mass_cart
+
+    @staticmethod
+    def decode_states(x):
+        x = np.squeeze(x)
+        return x[0], x[1], x[2], x[3]
+
+    @staticmethod
+    def decode_control(u):
+        u = np.squeeze(u)
+        return u
+
+    def derivatives(self, x, u):
+
+        angular_position, angular_velocity, _, velocity = self.decode_states(x)
+        force = self.decode_control(u)
+
+        angular_position_sin = np.sin(angular_position)
+        angular_position_cos = np.cos(angular_position)
+
+        angular_acceleration = (
+            self.gravity * angular_position_sin + angular_position_cos * (
+                (-force - self.mass_pendulum * self.length * angular_velocity**2 * angular_position_sin) / self.mass_total
+            )
+        ) / (
+            self.length * (
+                4/3 - self.mass_pendulum * angular_position_cos**2 / self.mass_total
+            )
+        ) - angular_velocity * self.angular_friction
+        
+        acceleration = (
+            force + self.mass_pendulum * self.length * (
+                angular_velocity**2 * angular_position_sin - angular_acceleration * angular_position_cos
+            ) 
+        ) / self.mass_total
+        
+        return np.array([
+            [angular_velocity],
+            [angular_acceleration],
+            [velocity],
+            [acceleration]
+        ])
+
+    def step(self, x, u, dt):
+        dx_dt = self.derivatives(x, u)
+        return x + dx_dt * dt
---
+++ tests/test_nonlinear_model.py
@@ -0,0 +1,37 @@
+import numpy as np
+import plotly.graph_objects as go
+
+from paddington.plants.nonlinear_model import InvertedPendulum
+
+problem = InvertedPendulum()
+
+angular_position_0 = np.deg2rad(150)
+angular_velocity_0 = 0.0
+position_0 = 0.0
+velocity_0 = 0.0
+
+states = np.array([
+    [angular_position_0],
+    [angular_velocity_0],
+    [position_0],
+    [velocity_0]
+])
+
+dt = 0.01
+states_log = [states]
+ts = np.arange(0, 1e4) * dt
+
+for t in ts:
+    states = problem.step(x=states, u=np.array([[0]]), dt=dt)
+    states_log.append(states)
+
+fig = go.Figure()
+
+fig.add_trace(
+    go.Scatter(
+        x=ts,
+        y=[np.rad2deg(x[0, 0]) for x in states_log]
+    )
+)
+
+fig.show()
