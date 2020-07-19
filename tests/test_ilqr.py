27b67684ed9a64fe244b4f08f6649692a67368b5
David Browne <davidbrowne@Davids-MacBook-Pro.local>
2020-07-19 14:38:00 -0700

    1-1 with previous iLQR test

--- tests/test_ilqr.py
+++ tests/test_ilqr.py
@@ -58,14 +58,14 @@ class TestILQR(unittest.TestCase):
         g_u = torch.tensor([[0.0]], dtype=torch.float64)
         self.cost_function = quadratic_cost_function(g_xx=g_xx, g_xu=g_xu, g_uu=g_uu, g_x=g_x, g_u=g_u)
 
-        dt = 0.025
+        dt = 0.1
 
         self.solver = iLQR(plant=self.plant, cost_function=self.cost_function, dt=dt)
 
     def test_ilqr(self):
 
-        states_initial = torch.tensor([[5.0], [0.0], [math.pi/8], [0.0]], dtype=torch.float64)
-        time_total = 10
+        states_initial = torch.tensor([[5.0], [0.0], [math.pi/4], [0.0]], dtype=torch.float64)
+        time_total = 40
         xs, us = self.solver.solve(states_initial, time_total)
 
         fig = go.Figure()
