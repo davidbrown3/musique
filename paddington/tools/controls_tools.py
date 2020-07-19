d866678779566e96dad254aa21445aa8ee22d557
David Browne <davidbrowne@Davids-MacBook-Pro.local>
2020-07-18 23:27:19 -0700

    Updating cost function methods

--- paddington/tools/controls_tools.py
+++ paddington/tools/controls_tools.py
@@ -22,23 +22,63 @@ class cost_function():
 
 class quadratic_cost_function(cost_function):
 
-    def __init__(self, Cx_diag, Cu_diag, cx, cu):
-        C_stacked = torch.cat((Cx_diag, Cu_diag))
-        self.C = torch.eye(len(C_stacked)) * C_stacked
-        self.c = torch.cat((cx, cu))
+    def __init__(self, g_xx, g_xu, g_uu, g_x, g_u):
+
+        if g_xx.shape[0] != g_xx.shape[1]:
+            raise ValueError('g_xx must have shape (N_x, N_x)')
+        if g_uu.shape[0] != g_uu.shape[1]:
+            raise ValueError('g_uu must have  shape (N_u, N_u)')
+        if g_xu.shape != (g_xx.shape[0], g_uu.shape[0]):
+            raise ValueError('g_xu must have  shape (N_x, N_u)')
+        if g_x.shape != (1, g_xx.shape[0]):
+            raise ValueError('g_x must have  shape (N_x, 1)')
+        if g_u.shape != (1, g_uu.shape[0]):
+            raise ValueError('g_u must have  shape (N_u, 1)')
+
+        self.g_xx = g_xx
+        self.g_xu = g_xu
+        self.g_uu = g_uu
+        self.g_x = g_x
+        self.g_u = g_u
+
+    def calculate_quadratic_cost(self, x, u):
+
+        return (torch.matmul(x.T, torch.matmul(self.g_xx, x)) +
+                2 * torch.matmul(x.T, torch.matmul(self.g_xu, u)) +
+                torch.matmul(u.T, torch.matmul(self.g_uu, u))) / 2
+
+    def calculate_linear_cost(self, x, u):
+
+        return torch.matmul(self.g_x, x) + torch.matmul(self.g_u, u)
 
     def calculate_cost(self, x, u):
-        stacked = torch.cat((x, u))
-        return torch.matmul(stacked.T, torch.matmul(self.C, stacked)) / 2
+        return self.calculate_quadratic_cost(x, u) + self.calculate_linear_cost(x, u)
 
-    def calculate_cost_hessian(self, x, u):
-        # For quadratic cost function, hessian is just the matrix
-        return self.C
+    def calculate_g_xx(self, x, u):
+        return self.g_xx
 
-    def calculate_cost_jacobian(self, x, u):
-        # For quadratic cost function, hessian is just the matrix
-        stacked = torch.cat((x, u))
-        return torch.matmul(self.C, stacked) + self.c
+    def calculate_g_xu(self, x, u):
+        return self.g_xu
+
+    def calculate_g_ux(self, x, u):
+        return self.calculate_g_xu(x, u).T
+
+    def calculate_g_uu(self, x, u):
+        return self.g_uu
+
+    def calculate_g_x(self, x, u):
+
+        g_xx = self.calculate_g_xx(x, u)
+        g_ux = self.calculate_g_ux(x, u)
+
+        return torch.matmul(g_xx, x).T + torch.matmul(u.T, g_ux).T + self.g_x
+
+    def calculate_g_u(self, x, u):
+
+        g_ux = self.calculate_g_ux(x, u)
+        g_uu = self.calculate_g_uu(x, u)
+
+        return torch.matmul(g_ux, x) + torch.matmul(g_uu, u) + self.g_u
 
 
 def diagonalize(diagonal):
--- tests/test_ilqr.py
+++ tests/test_ilqr.py
@@ -7,32 +7,40 @@ import torch
 from paddington.examples.models.nonlinear.inverted_pendulum import \
     InvertedPendulum
 from paddington.solvers.ilqr import iLQR
-from paddington.tools.controls_tools import quadratic_cost_function
+from paddington.tools.controls_tools import (diagonalize,
+                                             quadratic_cost_function)
 
 
 class TestQuadraticCost(unittest.TestCase):
 
     def setUp(self):
 
-        Cx_diag = torch.tensor([0.1, 0.0, 2.0, 0.0])
-        Cu_diag = torch.tensor([0.1])
-        cx = torch.tensor([[0.0], [0.0], [0.0], [0.0]])
-        cu = torch.tensor([[0.0]])
-        self.cost_function = quadratic_cost_function(Cx_diag, Cu_diag, cx, cu)
+        g_xx_0 = diagonalize(torch.tensor([0.1, 0.0, 2.0, 0.0], dtype=torch.float64))
+        g_xu_0 = torch.zeros([4, 1], dtype=torch.float64)
+        g_uu_0 = torch.tensor([[0.1]], dtype=torch.float64)
+        g_x_0 = torch.tensor([[0.0, 0.0, 0.0, 0.0]], dtype=torch.float64)
+        g_u_0 = torch.tensor([[0.0]], dtype=torch.float64)
+        self.cost_function = quadratic_cost_function(g_xx_0, g_xu_0, g_uu_0, g_x_0, g_u_0)
 
     def test_jacobian(self):
 
-        states_initial = torch.tensor([[-5.0], [0.0], [0.0], [0.0]])
-        controls_initial = torch.tensor([[0.0]])
-        jacobian_1 = self.cost_function.calculate_cost_jacobian(x=states_initial, u=controls_initial)
-        jacobian_2 = torch.autograd.functional.jacobian(self.cost_function.calculate_cost, (states_initial[:, 0], controls_initial[:, 0]))
+        states_initial = torch.tensor([[-5.0], [0.0], [0.0], [0.0]], dtype=torch.float64)
+        controls_initial = torch.tensor([[0.0]], dtype=torch.float64)
+        auto_jacobian = torch.autograd.functional.jacobian(self.cost_function.calculate_cost, (states_initial[:, 0], controls_initial[:, 0]))
 
-        [self.assertAlmostEqual(l, n, places=4) for l, n in zip(torch.squeeze(jacobian_1).tolist(), torch.squeeze(torch.cat(jacobian_2)).tolist())]
+        g_u = self.cost_function.calculate_g_u(x=states_initial, u=controls_initial)
+        g_x = self.cost_function.calculate_g_x(x=states_initial, u=controls_initial)
+
+        self.assertAlmostEqual(self.cost_function.calculate_quadratic_cost(x=states_initial, u=controls_initial).tolist()[0][0], 1.25)
+        self.assertAlmostEqual(self.cost_function.calculate_linear_cost(x=states_initial, u=controls_initial).tolist()[0][0], 0.0)
+
+        [self.assertAlmostEqual(l, n, places=4) for l, n in zip(auto_jacobian[1].tolist(), g_u.tolist())]
+        [self.assertAlmostEqual(l, n, places=4) for l, n in zip(auto_jacobian[0].tolist(), g_x.tolist())]
 
     def test_hessian(self):
 
-        states_initial = torch.tensor([[-5.0], [0.0], [0.0], [0.0]])
-        controls_initial = torch.tensor([[0.0]])
+        states_initial = torch.tensor([[-5.0], [0.0], [0.0], [0.0]], dtype=torch.float64)
+        controls_initial = torch.tensor([[0.0]], dtype=torch.float64)
         hessian_1 = self.cost_function.calculate_cost_hessian(x=states_initial, u=controls_initial)
         hessian_2 = torch.autograd.functional.hessian(self.cost_function.calculate_cost, (states_initial[:, 0], controls_initial[:, 0]))
 
