daa883fc47d341f3706d3d24b931648d2663a7a1
David Browne <davidbrowne@Davids-MacBook-Pro.local>
2020-08-15 00:26:55 -0700

    Adding 2nd derivatives

--- paddington/plants/nonlinear_model.py
+++ paddington/plants/nonlinear_model.py
@@ -1,7 +1,4 @@
 import jax
-import jax.numpy as np
-
-from paddington.tools.controls_tools import continuous_to_discrete
 
 
 class NonLinearModel():
@@ -13,19 +10,20 @@ class NonLinearModel():
         self.calculate_statespace = jax.jacfwd(self._derivatives, [0, 1])
         self.calculate_statespace_discrete = jax.jacfwd(self.step, [0, 1])
         self.calculate_statespace_discrete_batch = jax.vmap(self.calculate_statespace_discrete, in_axes=(0, 0))
-        # self.calculate_hessian_discrete = jax.jit(self._calculate_hessian_discrete)
-        # self.calculate_hessian_discrete_batch = jax.vmap(self.calculate_hessian_discrete, in_axes=(0, 0))
+        self._calculate_hessian_discrete = jax.hessian(self.step, [0, 1])
+        self.calculate_hessian_discrete = jax.jit(self._calculate_hessian_discrete)
+        self.calculate_hessian_discrete_batch = jax.vmap(self.calculate_hessian_discrete, in_axes=(0, 0))
         self._calculate_hessian = jax.hessian(self._derivatives, [0, 1])
         self.calculate_hessian = jax.jit(self._calculate_hessian)
         self.calculate_hessian_batch = jax.vmap(self.calculate_hessian, in_axes=(0, 0))
 
-    # @ property
-    # def N_x(self):
-    #     return 0
+    @ property
+    def N_x(self):
+        return 0
 
-    # @ property
-    # def N_u(self):
-    #     return 0
+    @ property
+    def N_u(self):
+        return 0
 
     def _step(self, x, u):
         dx_dt = self._derivatives(x, u)
--- paddington/solvers/ilqr.py
+++ paddington/solvers/ilqr.py
@@ -1,8 +1,6 @@
 import jax
 import jax.numpy as np
 
-from paddington.solvers.lqr import LQR
-
 
 class iLQR:
 
@@ -62,6 +60,12 @@ class iLQR:
         us = np.flip(us, axis=0)
 
         T_xs, T_us = self.plant.calculate_statespace_discrete_batch(xs, us)
+        T_hessians = self.plant.calculate_hessian_discrete_batch(xs, us)
+        T_xxs = T_hessians[0][0]
+        T_xus = T_hessians[0][1]
+        T_uxs = T_hessians[1][0]
+        T_uus = T_hessians[1][1]
+
         g_xs = self.running_cost_function.calculate_g_x_batch(xs, us)
         g_us = self.running_cost_function.calculate_g_u_batch(xs, us)
 
@@ -76,7 +80,7 @@ class iLQR:
         betas = np.empty([xs.shape[0], us.shape[1], xs.shape[1]])
         alphas = np.empty([us.shape[0], us.shape[1]])
 
-        data = (R_x, R_xx, T_xs, T_us, g_xs, g_us, g_xx, g_uu, g_xu, g_ux, betas, alphas)
+        data = (R_x, R_xx, T_xs, T_us, T_xxs, T_xus, T_uxs, T_uus, g_xs, g_us, g_xx, g_uu, g_xu, g_ux, betas, alphas)
         data = jax.lax.fori_loop(0, xs.shape[0], iLQR.backward_pass_inner, data)
 
         return np.flip(data[-2], axis=0), np.flip(data[-1], axis=0)
@@ -85,23 +89,60 @@ class iLQR:
     @ jax.jit
     def backward_pass_inner(i, data):
 
-        R_x, R_xx, T_xs, T_us, g_xs, g_us, g_xx, g_uu, g_xu, g_ux, betas, alphas = data
+        R_x, R_xx, T_xs, T_us, T_xxs, T_xus, T_uxs, T_uus, g_xs, g_us, g_xx, g_uu, g_xu, g_ux, betas, alphas = data
 
-        R_x, R_xx, beta, alpha = LQR.backward_pass(R_x=R_x,
-                                                   R_xx=R_xx,
-                                                   T_x=T_xs[jax.ops.index[i, :, :]],
-                                                   T_u=T_us[jax.ops.index[i, :, :]],
-                                                   g_x=g_xs[jax.ops.index[i, :]],
-                                                   g_u=g_us[jax.ops.index[i, :]],
-                                                   g_xx=g_xx,
-                                                   g_uu=g_uu,
-                                                   g_xu=g_xu,
-                                                   g_ux=g_ux)
+        Q_xx, Q_uu, Q_xu, Q_ux, Q_x, Q_u = iLQR.calculate_Q_partials(
+            R_x=R_x,
+            R_xx=R_xx,
+            T_x=T_xs[jax.ops.index[i, :, :]],
+            T_u=T_us[jax.ops.index[i, :, :]],
+            T_xx=T_xxs[jax.ops.index[i, :, :, :]],
+            T_xu=T_xus[jax.ops.index[i, :, :, :]],
+            T_ux=T_uxs[jax.ops.index[i, :, :, :]],
+            T_uu=T_uus[jax.ops.index[i, :, :, :]],
+            g_x=g_xs[jax.ops.index[i, :]],
+            g_u=g_us[jax.ops.index[i, :]],
+            g_xx=g_xx,
+            g_uu=g_uu,
+            g_xu=g_xu,
+            g_ux=g_ux
+        )
+
+        beta, alpha = iLQR.calculate_control_gains(Q_ux, Q_uu, Q_u)
+
+        # Return function constants
+        R_xx = Q_xx + np.matmul(Q_xu, beta) + np.matmul(beta.T, Q_ux) + np.matmul(beta.T, np.matmul(Q_uu, beta))
+        R_x = Q_x + np.matmul(Q_u, beta) + np.matmul(Q_xu, alpha).T + np.matmul(alpha.T, np.matmul(Q_uu, beta))
+
+        assert(R_x.shape[0] == 1)  # TODO: Replace with unittest
 
         betas = jax.ops.index_update(betas, jax.ops.index[i, :], beta)
         alphas = jax.ops.index_update(alphas, jax.ops.index[i, :], alpha.squeeze())
 
-        return (R_x, R_xx, T_xs, T_us, g_xs, g_us, g_xx, g_uu, g_xu, g_ux, betas, alphas)
+        return (R_x, R_xx, T_xs, T_us, T_xxs, T_xus, T_uxs, T_uus, g_xs, g_us, g_xx, g_uu, g_xu, g_ux, betas, alphas)
+
+    @staticmethod
+    @jax.jit
+    def calculate_Q_partials(R_x, R_xx, T_x, T_u, T_xx, T_xu, T_ux, T_uu, g_x, g_u, g_xx, g_uu, g_xu, g_ux):
+
+        # Q Value
+        Q_xx = g_xx + np.matmul(T_x.T, np.matmul(R_xx, T_x)) + np.tensordot(R_x, T_xx, 1)[0, :, :]
+        Q_uu = g_uu + np.matmul(T_u.T, np.matmul(R_xx, T_u)) + np.tensordot(R_x, T_uu, 1)[0, :, :]
+        Q_xu = g_xu + np.matmul(T_x.T, np.matmul(R_xx, T_u)) + np.tensordot(R_x, T_xu, 1)[0, :, :]
+        Q_ux = g_ux + np.matmul(T_u.T, np.matmul(R_xx, T_x)) + np.tensordot(R_x, T_ux, 1)[0, :, :]
+        Q_x = g_x + np.matmul(R_x, T_x)
+        Q_u = np.matmul(R_x, T_u)
+
+        return Q_xx, Q_uu, Q_xu, Q_ux, Q_x, Q_u
+
+    @staticmethod
+    @jax.jit
+    def calculate_control_gains(Q_ux, Q_uu, Q_u):
+
+        beta = np.linalg.solve(-Q_uu, Q_ux)
+        alpha = np.linalg.solve(-Q_uu, Q_u)
+
+        return beta, alpha
 
     def forward_pass(self, xi, xs, us, betas, alphas, line_alpha=0.01):
 
